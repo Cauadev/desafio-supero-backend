64bdacb26013ba041ca0fdd0c343ed63655132b9
CAUADEV <cauadev@gmail.com>
2021-07-06 23:32:51 -0300

    subindo base do projeto

---
+++ desafio-supero-backend/src/main/java/com/cauadev/desafiosuperobackend/DesafioSuperoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cauadev.desafiosuperobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSuperoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSuperoBackendApplication.class, args);
+	}
+
+}
---
+++ desafio-supero-backend/src/test/java/com/cauadev/desafiosuperobackend/DesafioSuperoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauadev.desafiosuperobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSuperoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
