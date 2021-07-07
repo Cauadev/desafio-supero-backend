e304d89c8b95a781874f0d825af56b81ae9c91d4
CAUADEV <cauadev@gmail.com>
2021-07-07 00:26:35 -0300

    corrigindo controller

--- desafio-supero-backend/src/main/java/com/cauadev/desafiosuperobackend/controller/TaskController.java
+++ desafio-supero-backend/src/main/java/com/cauadev/desafiosuperobackend/controller/TaskController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,9 +30,12 @@ public class TaskController {
 	@Autowired
 	private TaskRepository repository;
 	
+	@PostMapping
 	public ResponseEntity<Task> createTask(@RequestBody Task task) {
+		
 		task.setDate(LocalDateTime.now());
 		task.setStage(Stage.NO_COMPLETED);
+		
 		return ResponseEntity.ok(repository.save(task));
 	}
 	
@@ -50,6 +54,7 @@ public class TaskController {
 						.withSelfRel());
 			});
 		}
+		
 		return ResponseEntity.ok(task_list);
 	}
 	
@@ -68,6 +73,7 @@ public class TaskController {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteTask(@PathVariable Long id){
 		repository.deleteById(id);
+		
 		return ResponseEntity.ok("success");
 	}
 	
@@ -79,9 +85,11 @@ public class TaskController {
 	@PatchMapping("/{id}")
 	public ResponseEntity<Task> updateStage(@PathVariable Long id){
 		Task task = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "task not found"));
+		
 		if(task.getStage() == Stage.COMPLETED) {
 			task.setStage(Stage.NO_COMPLETED);
 		}else task.setStage(Stage.COMPLETED);
+		
 		return ResponseEntity.ok(task);
 	}
 
